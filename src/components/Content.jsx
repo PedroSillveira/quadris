7e3e17697d7e832e3a139536fc0eab58dbb3a326
PedroSillveira <pedrosilveira.profissional@gmail.com>
2024-09-24 23:10:28 -0300

    update content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,20 +6,26 @@ import './content.css';
 
 export const Content = () => {
   return (
-    <div className='content'>
-        <Container>
-      <Row>
-        <Col>
-            <h1>VIVA UMA EXPERIÊNCIA INESQUECIVEL</h1>
-            <p>
-                Visite Barra Grande Piauí, um dos destinos mais desejados do litoral Nordestino e aproveite para viver momentos únicos de aventura e emoção pelas melhores praias de paradisíacas. Nós disponibilizamos quadriciclos Off-Road 4×4 revisados para realizar os melhores passeios pelas praias mais cobiçadas do Piauí com quadriciclo, a emoção e aventura será garantida, além de poder desfrutar das belas paisagens presentes.
-            </p>    
-        </Col>
-        <Col>
-            FOTOS POLAROID
-        </Col>
-      </Row>
-    </Container>
+    <div className='content p-5'>
+      <Container>
+        <Row className=''>
+          <Col xs={12} md={6}>
+            <h1 className='my-5'>VIVA UMA EXPERIÊNCIA INESQUECÍVEL</h1>
+            <p className='my-5'>
+              Visite Barra Grande Piauí, um dos destinos mais desejados do litoral Nordestino e aproveite para viver momentos únicos de aventura e emoção pelas melhores praias paradisíacas. Nós disponibilizamos quadriciclos Off-Road 4×4 revisados para realizar os melhores passeios pelas praias mais cobiçadas do Piauí com quadriciclo, a emoção e aventura será garantida, além de poder desfrutar das belas paisagens presentes.
+            </p>
+          </Col>
+          <Col xs={12} md={6}>
+            <div>
+              <img 
+                src="images/image03.png" 
+                alt="fotos de paisagem" 
+                className='my-5 polaroid'
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,80 +8,73 @@ import './main.css';
 
 export const Main = () => {
   return (
-    <div className='main'>
+    <div className='main'>  
         <div className='aluguel'>
-          <h1 className=''>ALUGUEL DE QUADRICICLO</h1>
-          <p>
-            Liberte-se da  rotina que te prende e venha para Barra Grande curtir os melhores passeios pilotando Quadriciclo 4×4, aventurando-se com Quadris da Bara.
-            Emoção garantida com as melhores tarifas de região!
-          </p>
-          <div className='aluguelCards'>
-            <Container>
-              <Row>
-                <Col>
-                  <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="/images/image02.png" />
-                    <Card.Body>
-                      <Card.Title>Manhã ou Tarde</Card.Title>
-                      <Card.Text>
-                        <ul>
-                          <li>
-                            locação por 04 horas
-                          </li>
-                          <li>
-                            Tanque cheio
-                          </li>
-                          <li>
-                            Entrega na sua localização
-                          </li>
-                        </ul>
-                      </Card.Text>
-                      <Button variant="primary">Saiba mais</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="/images/image02.png" />
-                    <Card.Body>
-                      <Card.Title>Diária</Card.Title>
-                      <Card.Text>
-                        <ul>
-                          <li>
-                            locação por 24 horas
-                          </li>
-                          <li>
-                            Tanque cheio
-                          </li>
-                          <li>
-                            Entrega na sua localização
-                          </li>
-                        </ul>
-                      </Card.Text>
-                      <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="/images/image02.png" />
-                    <Card.Body>
-                      <Card.Title>Locação maior que 02 diárias ou para eventos</Card.Title>
-                      <Card.Text>
-                       <ul>
-                          <li>
-                            Valor varia de acordo com a quantidade de dias locado, entre em contato e solicite seu orçamento! 
-                          </li>
-                          
-                        </ul>
-                      </Card.Text>
-                      <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Container>           
+          <div className='textA'>
+            <h1 className='text-center'>ALUGUEL DE QUADRICICLO</h1>
+            <p className='text-center mb-5  '>
+              Liberte-se da  rotina que te prende e venha para Barra Grande curtir os melhores passeios pilotando Quadriciclo 4×4, aventurando-se com Quadris da Bara.
+              Emoção garantida com as melhores tarifas de região!
+            </p>
           </div>
+            <div className='aluguelCards mb-5'>
+              <Container>
+                <Row className="justify-content-center">
+                  <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+                    <Card>
+                      <Card.Img variant="top" src="/images/image02.png" />
+                      <Card.Body>
+                        <Card.Title className='text-center'>Manhã ou Tarde</Card.Title>
+                        <Card.Text>
+                          <ul>
+                            <li>locação por 04 horas</li>
+                            <li>Tanque cheio</li>
+                            <li>Entrega na sua localização</li>
+                          </ul>
+                        </Card.Text>
+                        <div className='d-flex justify-content-center'>
+                          <Button variant="primary" >Go somewhere</Button>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+                    <Card>
+                      <Card.Img variant="top" src="/images/image02.png" />
+                      <Card.Body>
+                        <Card.Title className='text-center'>Diária</Card.Title>
+                        <Card.Text>
+                          <ul>
+                            <li>locação por 24 horas</li>
+                            <li>Tanque cheio</li>
+                            <li>Entrega na sua localização</li>
+                          </ul>
+                        </Card.Text>
+                        <div className='d-flex justify-content-center'>
+                          <Button variant="primary" >Go somewhere</Button>
+                        </div>                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col xs={12} sm={6} md={4} lg={3} className="mb-4">
+                    <Card>
+                      <Card.Img variant="top" src="/images/image02.png" />
+                      <Card.Body>
+                        <Card.Title className='text-center'>Locação maior que 02 diárias</Card.Title>
+                        <Card.Text>
+                          <ul>
+                            <p>Valor varia de acordo com a quantidade de diárias</p>
+                          </ul>
+                        </Card.Text>
+                        <div className='d-flex justify-content-center'>
+                          <Button variant="primary" >Go somewhere</Button>
+                        </div>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+
         </div>
     </div>
   )
